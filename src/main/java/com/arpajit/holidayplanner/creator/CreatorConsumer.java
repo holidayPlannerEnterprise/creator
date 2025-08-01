678b8573514ae8b81d54235b945dfd1c5da5f80e
arpa2001 <arpajitofficial@gmail.com>
2025-08-01 11:33:09 +0530

    Trying replyKafka

--- src/main/java/com/arpajit/holidayplanner/creator/CreatorConsumer.java
+++ src/main/java/com/arpajit/holidayplanner/creator/CreatorConsumer.java
@@ -3,7 +3,10 @@ package com.arpajit.holidayplanner.creator;
 import org.slf4j.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -19,8 +22,14 @@ public class CreatorConsumer {
     @Autowired
     private CreatorDataServComm dataService;
 
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
     @KafkaListener(topics = "holidayplanner-creator", groupId = "holidayplanner-controller")
-    public void consumedPayload(String payload, Acknowledgment act) throws Exception {
+    public void consumedPayload(String payload,
+                                @Header(KafkaHeaders.REPLY_TOPIC) String replyTopic,
+                                Acknowledgment act)
+                                throws Exception {
         logger.info("Received Kafka response: {}", payload);
         act.acknowledge();
         ConsumeMessage message = objectMapper.readValue(payload, ConsumeMessage.class);
@@ -29,11 +38,14 @@ public class CreatorConsumer {
         switch (message.getRequestType()) {
             case "GET_ALL_HOLIDAYS":
                 logger.info("Triggering GET_ALL_HOLIDAYS service");
+                String response = "{" + "Status" + ": " + "ok" + "}";
+                dataService.updateAudit(message.getTraceId(), "CONTROLLER_SENT", null);
+                kafkaTemplate.send(replyTopic, response);
                 break;
             default:
                 logger.warn("{} does not match any request type", message.getRequestType());
                 dataService.updateAudit(message.getTraceId(),
-                                        "CREATOR_RECEIVED",
+                                        "FAILED_AT_CREATOR",
                                         "Request type: "+message.getRequestType()+" not matched in Consumer");
                 break;
         }
